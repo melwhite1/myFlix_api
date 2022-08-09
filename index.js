2c9cf26029f00b64d3145ddaf95eab1f6641f2d3
melwhite1 <melissawhite668@gmail.com>
2022-08-09 15:40:21 -0400

    Remove silly mistake

--- index.js
+++ index.js
@@ -247,5 +247,3 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
-
-mongosh "mongodb+srv://myflixdb.ddi86kf.mongodb.net/myFlixDB" --apiVersion 1 --username melissawhite668
