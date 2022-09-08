7d5be6304fe2ceb9671524084c2e0fced305f60a
melwhite1 <melissawhite668@gmail.com>
2022-09-08 13:26:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -173,14 +173,14 @@ app.put('/users/:Username',
 
 
 //Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', function (req, res) {
   Movies.find()
-    .then((movies) => {
+    .then(function (movies) {
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
     });
 });
 
